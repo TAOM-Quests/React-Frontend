e4a03b6e3b349d9ee652217f338e19e2c10a34ab
Roman Nichipurenko <roman.nichi.o@gmail.com>
2025-03-30 14:21:47 +0400

    tq-5: Add msw env

---
+++ src/mocks/msw.ts
@@ -0,0 +1,4 @@
+import { setupWorker } from "msw/browser";
+import { apiHandlers } from "./api/apiHandlers";
+
+export const worker = setupWorker(...apiHandlers);
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: './src/env',
   test: {
     testTimeout: 500_000,
     hookTimeout: 1000_000
