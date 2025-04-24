11f98b7539335c623a3606a1ab7edc95e4a80b24
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-04-24 21:50:05 +0400

    tq-30: Регламент

--- src/pages/EventCreate/EventCreateSchedule/EventCreateSchedule.tsx
+++ src/pages/EventCreate/EventCreateSchedule/EventCreateSchedule.tsx
@@ -3,6 +3,9 @@ import { Button } from '../../../components/UI/Button/Button'
 import { Icon } from '../../../components/UI/Icon/Icon'
 import Input from '../../../components/UI/Input/Input'
 import { ScheduleItem } from '../../../models/event'
+import './EventCreateSchedule.scss'
+import { TimeInput } from '../../../components/UI/TimeInput/TimeInput'
+import moment from 'moment'
 
 export interface EventCreateScheduleProps {
   schedule: ScheduleItem[]
@@ -13,6 +16,7 @@ export const EventCreateSchedule = ({
   schedule,
   setSchedule,
 }: EventCreateScheduleProps) => {
+  const TIME_FORMAT = 'HH:mm'
   const changeScheduleItem = (
     field: keyof ScheduleItem,
     data: any,
@@ -27,7 +31,19 @@ export const EventCreateSchedule = ({
         return scheduleItem
       }),
     )
+  const handleTimeSelect = (
+    field: keyof ScheduleItem,
+    time: string,
+    index: number,
+  ) => {
+    const newDate = moment()
+      .hour(moment(time, TIME_FORMAT).hour())
+      .minute(moment(time, TIME_FORMAT).minute())
+      .second(0)
+      .toDate()
 
+    changeScheduleItem(field, newDate, index)
+  }
   const addScheduleItem = () =>
     setSchedule(prevSchedule => [
       ...prevSchedule,
@@ -41,35 +57,50 @@ export const EventCreateSchedule = ({
 
   return (
     <div className="schedule">
-      {schedule.map((item, index) => (
-        <div className="schedule-item" key={`schedule-item-${index}`}>
-          <Input
-            iconAfter="TIME"
-            value={new Date(item.timeStart).getTime()}
-            onChange={e =>
-              changeScheduleItem('timeStart', new Date(e.target.value), index)
-            }
-          />
-          <Input
-            iconAfter="TIME"
-            value={new Date(item.timeEnd).getTime()}
-            onChange={e =>
-              changeScheduleItem('timeEnd', new Date(e.target.value), index)
-            }
-          />
-          <Input
-            value={item.name}
-            onChange={e => changeScheduleItem('name', e.target.value, index)}
-          />
-          <Icon icon="DELETE" onClick={() => removeScheduleItem(index)} />
-        </div>
-      ))}
-      <Button
-        text="Добавить"
-        iconBefore="ADD"
-        colorType={'secondary'}
-        onClick={addScheduleItem}
-      />
+      <label className="body_s_sb label">Регламент</label>
+      <div className="schedule__container-items">
+        {schedule.map((item, index) => (
+          <div className="schedule--item" key={`schedule-item-${index}`}>
+            <div className="schedule--time">
+              <TimeInput
+                key={index}
+                value={moment(item.timeStart).format(TIME_FORMAT)}
+                onTimeSelect={(time: string) =>
+                  handleTimeSelect('timeStart', time, index)
+                }
+              />
+              <TimeInput
+                key={index}
+                value={moment(item.timeEnd).format(TIME_FORMAT)}
+                onTimeSelect={(time: string) =>
+                  handleTimeSelect('timeEnd', time, index)
+                }
+              />
+            </div>
+
+            <div className="schedule--itemName">
+              <Input
+                value={item.name}
+                placeholder="Введите название события"
+                onChange={e =>
+                  changeScheduleItem('name', e.target.value, index)
+                }
+              />
+              <Icon icon="DELETE" onClick={() => removeScheduleItem(index)} />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <Button
+          text="Добавить"
+          iconBefore="ADD"
+          colorType={'secondary'}
+          size="small"
+          onClick={addScheduleItem}
+        />
+      </div>
     </div>
   )
 }
