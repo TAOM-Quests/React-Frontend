84f2968edd2f0998f47ba953d117f6e758e8cc50
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-05-01 11:17:43 +0400

    tq-48: office_number

--- src/pages/Event/EventOfflinePlace/EventOfflinePlace.tsx
+++ src/pages/Event/EventOfflinePlace/EventOfflinePlace.tsx
@@ -1,17 +1,21 @@
 import { ContainerBox } from '../../../components/ContainerBox/ContainerBox'
 import { Icon } from '../../../components/UI/Icon/Icon'
 import { PlaceOffline } from '../../../models/event'
-
+import './EventOfflinePlace.scss'
 export interface EventOfflinePlaceProps {
   place: PlaceOffline
 }
 
 export const EventOfflinePlace = ({ place }: EventOfflinePlaceProps) => {
   return (
-    <ContainerBox>
-      <Icon icon="DEPARTMENT" />
-      <h2>Этаж {place.floor}</h2>
-      <p>{place.office_number}</p>
+    <ContainerBox className="event-offline">
+      <Icon icon="DEPARTMENT" colorIcon="subdued" size="Large" />
+      <div className="event-offline-place">
+        <h6 className="heading_6">Этаж {place.floor}</h6>
+        <p className="heading_5 office_number">
+          {place.office_number} Felbnjhbz
+        </p>
+      </div>
     </ContainerBox>
   )
 }
