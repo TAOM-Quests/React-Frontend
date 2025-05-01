2918d3986d8934db2eec4995a9e9064d1fbdc18d
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-05-01 11:03:15 +0400

    tq-48: EventFiles

--- src/pages/Event/EventFiles/EventFiles.tsx
+++ src/pages/Event/EventFiles/EventFiles.tsx
@@ -8,6 +8,7 @@ export interface EventFilesProps {
 export const EventFiles = ({ files }: EventFilesProps) => {
   return (
     <ContainerBox>
+      <h5 className="heading_5">Mатериалы</h5>
       {files.map(file => (
         <a key={file.id} href={file.url}>
           {file.originalName}.{file.extension}
