e6345c0b585ffab9c46ab01da5c9e70bad89c6d0
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-05-01 23:05:54 +0400

    tq-48: add CardFile  - EventFiles

--- src/pages/Event/EventFiles/EventFiles.tsx
+++ src/pages/Event/EventFiles/EventFiles.tsx
@@ -1,3 +1,4 @@
+import { CardFile } from '../../../components/CardFile/CardFile'
 import { ContainerBox } from '../../../components/ContainerBox/ContainerBox'
 import { ServerFile } from '../../../models/serverFile'
 
@@ -10,9 +11,13 @@ export const EventFiles = ({ files }: EventFilesProps) => {
     <ContainerBox>
       <h5 className="heading_5">Mатериалы</h5>
       {files.map(file => (
-        <a key={file.id} href={file.url}>
-          {file.originalName}.{file.extension}
-        </a>
+        <CardFile
+          key={file.id}
+          url={file.url}
+          size={file.size}
+          fileName={file.originalName}
+          extension={file.extension}
+        />
       ))}
     </ContainerBox>
   )
