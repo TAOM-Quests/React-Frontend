afbc6019267f995451bb77d75ae5f2a5dc61dd52
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-05-01 10:58:00 +0400

    tq-48: EventMainData

--- src/pages/Event/EventMainData/EventMainData.tsx
+++ src/pages/Event/EventMainData/EventMainData.tsx
@@ -10,6 +10,8 @@ import { useAppSelector } from '../../../hooks/redux/reduxHooks'
 import { selectAuth } from '../../../redux/auth/authSlice'
 import { useNavigate } from 'react-router'
 import { Participant } from '../../../models/user'
+import './EventMainData.scss'
+import { EventType } from '../../../models/eventType'
 
 export interface EventMainDataProps {
   eventId: number
@@ -19,6 +21,7 @@ export interface EventMainDataProps {
   image?: ServerFile
   participants?: Participant[]
   places?: (PlaceOnline | PlaceOffline)[]
+  type?: EventType
 }
 
 export const EventMainData = ({
@@ -29,6 +32,7 @@ export const EventMainData = ({
   eventId,
   department,
   participants,
+  type,
 }: EventMainDataProps) => {
   const navigate = useNavigate()
   const user = useAppSelector(selectAuth)
@@ -52,35 +56,58 @@ export const EventMainData = ({
   }
 
   return (
-    <ContainerBox>
-      <img src={image?.url} />
-      <h1>{name}</h1>
-      <div>
-        <Icon icon="CALENDAR" />
-        <span>{moment(date).format('DD MMMM YYYY год')}</span>
+    <div
+      className="event-banner"
+      style={{
+        backgroundImage: image ? `url(${image.url})` : undefined,
+      }}
+    >
+      <div className="event-banner__overlay" />
+      <div className="event-banner__content">
+        <h3 className="heading_3 event-banner__title">{name}</h3>
+        <div className="event-banner__info-rows">
+          <div className="body_xl_m event-banner__info-row">
+            <Icon icon="CALENDAR" colorIcon="subdued" />
+            <span>{moment(date).format('DD MMMM YYYY год')}</span>
+          </div>
+          <div className="body_xl_m event-banner__info-row">
+            <Icon icon="TIME" colorIcon="subdued" />
+            <span>{moment(date).format('HH:mm')}</span>
+          </div>
+          {placeOffline?.address && (
+            <div className="body_xl_m event-banner__info-row">
+              <Icon icon="MARKER_MAP" colorIcon="subdued" />
+              <span>{placeOffline?.address}</span>
+            </div>
+          )}
+
+          {placeOnline?.platform && (
+            <div className="body_xl_m event-banner__info-row">
+              <Icon icon="PLATFORM" colorIcon="subdued" />
+              <span>{placeOnline?.platform}</span>
+            </div>
+          )}
+          <div className="body_xl_m event-banner__info-row">
+            <Icon icon="DEPARTMENT" colorIcon="subdued" />
+            <span>{department.name}</span>
+          </div>
+          <div className="body_xl_m event-banner__info-row">
+            <Icon icon="GRADUATION_CAP" colorIcon="subdued" />
+            <span>{type?.name}</span>
+          </div>
+        </div>
+
+        {user &&
+        !participants?.map(participant => participant.id).includes(user.id) ? (
+          <div>
+            <Button colorType="secondary" text="Вы зарегистрированы" disabled />
+          </div>
+        ) : (
+          <div>
+            <Button text="Участвовать" onClick={addParticipant} />
+          </div>
+        )}
       </div>
-      <div>
-        <Icon icon="TIME" />
-        <span>{moment(date).format('HH:mm')}</span>
-      </div>
-      <div>
-        <Icon icon="MARKER_MAP" />
-        <span>{placeOffline?.address}</span>
-      </div>
-      <div>
-        <Icon icon="PLATFORM" />
-        <span>{placeOnline?.platform}</span>
-      </div>
-      <div>
-        <Icon icon="DEPARTMENT" />
-        <span>{department.name}</span>
-      </div>
-      {user &&
-      !participants?.map(participant => participant.id).includes(user.id) ? (
-        <Button colorType="secondary" text="Вы зарегистрированы" disabled />
-      ) : (
-        <Button text="Участвовать" onClick={addParticipant} />
-      )}
-    </ContainerBox>
+    </div>
   )
 }
