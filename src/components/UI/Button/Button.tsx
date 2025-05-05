f838d5e2d467ccc51b45e795b50dbccb42646074
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-05-05 12:17:25 +0400

    tq-58: стили Конструктор квестов

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -35,6 +35,7 @@ export const Button = ({
     <button
       className={classNames(
         'body_m_b',
+        'text_ellipsis',
         'button',
         typeof colorType === 'string' ? `button--${colorType}` : '',
         `button--${size}`,
