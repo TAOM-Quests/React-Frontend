ed2ed6e06e6d5509705ecf512d26f517fbea1f83
Roman Nichipurenko <roman.nichi.o@gmail.com>
2025-05-05 21:31:12 +0400

    tq-46: Add image yo auth

--- src/models/userAuth.ts
+++ src/models/userAuth.ts
@@ -1,8 +1,15 @@
+import { ServerFile } from './serverFile'
+
 export interface UserAuth {
   id: number
   email: string
   token: string
+  name?: string
   isAdmin?: boolean
+  image?: ServerFile
   isEmployee?: boolean
+}
+
+export interface EmployeeAuth extends UserAuth {
   departmentId?: number
 }
