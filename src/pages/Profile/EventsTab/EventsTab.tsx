6503050d169677675f7160c91a1d1a15d0e2bf53
Roman Nichipurenko <roman.nichi.o@gmail.com>
2025-05-11 22:40:43 +0400

    tq-74: Исправляет фильтр

--- src/pages/Profile/EventsTab/EventsTab.tsx
+++ src/pages/Profile/EventsTab/EventsTab.tsx
@@ -15,6 +15,11 @@ import './EventsTab.scss'
 import { Button } from '../../../components/UI/Button/Button'
 import { useNavigate } from 'react-router'
 import { ScrollController } from '../../../components/ScrollController/ScrollController'
+import { EventType } from '../../../models/eventType'
+import { EventStatus } from '../../../models/eventStatus'
+import { isArray } from 'lodash'
+import { Loading } from '../../../components/Loading/Loading'
+
 interface EventsFilter {
   name?: string
   type?: number
@@ -26,14 +31,36 @@ export interface EventsTabProps {
 }
 
 export default function EventsTab({ user }: EventsTabProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [eventTypes, setEventTypes] = useState<EventType[]>([])
+  const [eventStatuses, setEventStatuses] = useState<EventStatus[]>([])
+
   const [filter, setFilter] = useState<EventsFilter>({})
   const [userEvents, setEvents] = useState<EventMinimize[]>([])
 
   const navigate = useNavigate()
 
   useEffect(() => {
+    setIsLoading(true)
+    fetchFilterData()
+    setIsLoading(false)
+  }, [])
+
+  useEffect(() => {
+    setIsLoading(true)
+    setEvents([])
     fetchEvents()
+    setIsLoading(false)
   }, [filter])
+
+  const fetchFilterData = async () => {
+    try {
+      setEventTypes(await events.getTypes())
+      setEventStatuses(await events.getStatuses())
+    } catch (e) {
+      console.log(`[EventsTab] ${e}`)
+    }
+  }
 
   const fetchEvents = async () => {
     const fetchedEvents = user.isEmployee
@@ -51,121 +78,97 @@ export default function EventsTab({ user }: EventsTabProps) {
     setEvents([...(userEvents ?? []), ...fetchedEvents])
   }
 
-  const getUniqueDropdownItems = (
-    events: EventMinimize[] | null,
-    selector: (
-      event: EventMinimize,
-    ) => { id: number; name: string } | undefined,
-  ): DropdownItemType[] => {
-    if (!events) return []
-
-    const uniqueMap = new Map<number, { id: number; name: string }>()
-
-    events
-      .filter(event => {
-        const item = selector(event)
-        return !!item && !!item.name
-      })
-      .forEach(event => {
-        const item = selector(event)!
-        if (!uniqueMap.has(item.id)) {
-          uniqueMap.set(item.id, item)
-        }
-      })
-
-    return Array.from(uniqueMap.values()).map(({ id, name }) => ({
-      id,
-      text: name,
-    }))
-  }
-
-  const dropdownTypes = getUniqueDropdownItems(userEvents, e => e.type)
-  const dropdownStatuses = getUniqueDropdownItems(userEvents, e => e.status)
-
-  const createDropdownChangeHandler =
-    (key: string, setFilter: Dispatch<React.SetStateAction<EventsFilter>>) =>
-    (selectedItem: DropdownItemType | DropdownItemType[] | null) => {
-      if (selectedItem === null) {
-        setFilter(state => ({ ...state, [key]: undefined }))
-        return
-      }
-
-      if (Array.isArray(selectedItem)) {
-        const first = selectedItem[0]
-        setFilter(state => ({ ...state, [key]: first?.id ?? undefined }))
-      } else {
-        setFilter(state => ({ ...state, [key]: selectedItem.id }))
-      }
-    }
-
   return (
-    <div className="profile_events">
-      <div className="profile_events--filters">
-        <Input
-          value={filter.name}
-          iconBefore="SEARCH"
-          placeholder="Поиск по названию"
-          onChange={e =>
-            setFilter(state => ({ ...state, name: e.target.value }))
-          }
-        />
-        <Dropdown
-          items={dropdownTypes}
-          placeholder="Тип мероприятия"
-          onChangeDropdown={createDropdownChangeHandler('type', setFilter)}
-        />
-        {user.isEmployee && (
-          <>
+    <>
+      {!isLoading ? (
+        <div className="profile_events">
+          <div className="profile_events--filters">
+            <Input
+              value={filter.name}
+              iconBefore="SEARCH"
+              placeholder="Поиск по названию"
+              onChange={e =>
+                setFilter(state => ({ ...state, name: e.target.value }))
+              }
+            />
             <Dropdown
-              items={dropdownStatuses}
-              placeholder="Статус"
-              onChangeDropdown={createDropdownChangeHandler(
-                'status',
-                setFilter,
-              )}
-            />
-
-            <Button
-              onClick={() => navigate('/event/create')}
-              text="Создать мероприятие"
-              colorType="primary"
-            />
-          </>
-        )}
-      </div>
-      <ScrollController
-        onEndScroll={fetchEvents}
-        className="profile_events--events"
-        style={{ overflow: 'scroll' }}
-      >
-        {userEvents && userEvents.length
-          ? userEvents.map(event => {
-              const onlinePlace: PlaceOnline | null =
-                event.places?.find(place => place.is_online) ?? null
-              const offlinePlace: PlaceOffline | null =
-                event.places?.find(place => !place.is_online) ?? null
-
-              const eventData: EventMinimizeProps = {
-                id: event.id,
-                date: event.date ?? null,
-                status: event.status.name,
-                name: event.name ?? '',
-                type: event.type?.name ?? '',
-                address: offlinePlace?.address ?? '',
-                platform: onlinePlace?.platform ?? '',
-                imageUrl: event.image?.url ?? '',
+              items={eventTypes.map(type => ({
+                id: type.id,
+                text: type.name,
+              }))}
+              placeholder="Тип мероприятия"
+              onChangeDropdown={selected =>
+                setFilter(state =>
+                  !isArray(selected) && selected
+                    ? { ...state, type: selected.id }
+                    : state,
+                )
               }
+            />
+            {user.isEmployee && (
+              <>
+                <Dropdown
+                  items={eventStatuses.map(status => ({
+                    id: status.id,
+                    text: status.name,
+                  }))}
+                  placeholder="Статус"
+                  onChangeDropdown={selected =>
+                    setFilter(state =>
+                      !isArray(selected) && selected
+                        ? { ...state, status: selected.id }
+                        : state,
+                    )
+                  }
+                />
 
-              if (user.isEmployee) {
-                eventData.isEmployeeView = true
-                eventData.onDelete = () =>
-                  setEvents(userEvents.filter(e => e.id !== event.id))
-              }
+                <Button
+                  onClick={() => navigate('/event/create')}
+                  text="Создать мероприятие"
+                  colorType="primary"
+                />
+              </>
+            )}
+          </div>
+          <ScrollController
+            onEndScroll={fetchEvents}
+            className="profile_events--events"
+            style={{ overflow: 'scroll' }}
+          >
+            {userEvents && userEvents.length
+              ? userEvents.map(event => {
+                  const onlinePlace: PlaceOnline | null =
+                    event.places?.find(place => place.is_online) ?? null
+                  const offlinePlace: PlaceOffline | null =
+                    event.places?.find(place => !place.is_online) ?? null
 
-              return <EventMinimizeComponent key={event.id} {...eventData} />
-            })
-          : 'Мероприятий нет'}
-      </ScrollController>
-    </div>
+                  const eventData: EventMinimizeProps = {
+                    id: event.id,
+                    date: event.date ?? null,
+                    status: event.status.name,
+                    name: event.name ?? '',
+                    type: event.type?.name ?? '',
+                    address: offlinePlace?.address ?? '',
+                    platform: onlinePlace?.platform ?? '',
+                    imageUrl: event.image?.url ?? '',
+                  }
+
+                  if (user.isEmployee) {
+                    eventData.isEmployeeView = true
+                    eventData.onDelete = () =>
+                      setEvents(userEvents.filter(e => e.id !== event.id))
+                  }
+
+                  return (
+                    <EventMinimizeComponent key={event.id} {...eventData} />
+                  )
+                })
+              : 'Мероприятий нет'}
+          </ScrollController>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   )
 }
