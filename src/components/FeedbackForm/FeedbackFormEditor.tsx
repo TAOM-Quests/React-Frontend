fe836e84d0bbc06e2e6cf7b4ac399c73da4acede
Roman Nichipurenko <roman.nichi.o@gmail.com>
2025-05-31 16:22:48 +0400

    tq-83: Add ref feedback

--- src/components/FeedbackForm/FeedbackFormEditor.tsx
+++ src/components/FeedbackForm/FeedbackFormEditor.tsx
@@ -1,49 +1,123 @@
-import { useState } from 'react'
+import { forwardRef, useEffect, useImperativeHandle, useState } from 'react'
 import { FormHeaderEditor } from './FormHeaderEditor'
 import { QuestionList } from './QuestionList'
-import { FeedbackForm, Question } from './feedback'
+import { FeedbackForm } from '../../models/feedbackForm'
+import { FeedbackQuestion } from '../../models/feedbackQuestion'
+import { Loading } from '../Loading/Loading'
+import { feedback } from '../../services/api/commonModule/commonEntities/feedback/feedback'
 
 const DEFAULT_TITLE = 'Обратная связь'
 const DEFAULT_DESCRIPTION =
   'Спасибо за участие! Пожалуйста, оставьте свой отзыв, чтобы мы могли улучшить нашу работу!'
 
 interface FeedbackFormEditorProps {
-  entity: string
+  entityName: string
   entityId: number | null
-  baseQuestions: Question[]
+  baseQuestions: FeedbackQuestion[]
   onFormSaved?: (form: FeedbackForm) => void
 }
 
-export const FeedbackFormEditor = ({
-  entity,
-  entityId,
-  baseQuestions,
-  onFormSaved,
-}: FeedbackFormEditorProps) => {
-  const [formId, setFormId] = useState<number | undefined>(undefined)
-  const [title, setTitle] = useState<string>(DEFAULT_TITLE)
-  const [description, setDescription] = useState<string>(DEFAULT_DESCRIPTION)
-  const [questions, setQuestions] = useState<Question[]>(baseQuestions)
+export const FeedbackFormEditor = forwardRef(
+  (
+    {
+      entityId,
+      entityName,
+      baseQuestions,
+      onFormSaved,
+    }: FeedbackFormEditorProps,
+    ref,
+  ) => {
+    const [title, setTitle] = useState<string>(DEFAULT_TITLE)
+    const [description, setDescription] = useState<string>(DEFAULT_DESCRIPTION)
+    const [questions, setQuestions] =
+      useState<FeedbackQuestion[]>(baseQuestions)
 
-  const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
-  const handleHeaderChange = (changes: {
-    title?: string
-    description?: string
-  }) => {
-    if (changes.title !== undefined) setTitle(changes.title)
-    if (changes.description !== undefined) setDescription(changes.description)
-  }
+    let formId: number | null = null
 
-  return (
-    <div>
-      <h2>Редактор формы обратной связи</h2>
-      <FormHeaderEditor
-        title={title}
-        description={description}
-        onChange={handleHeaderChange}
-      />
-      <QuestionList questions={questions} onChangeQuestions={setQuestions} />
-    </div>
-  )
-}
+    useImperativeHandle(
+      ref,
+      () => ({
+        saveForm,
+      }),
+      [],
+    )
+
+    useEffect(() => {
+      setIsLoading(true)
+      try {
+        fetchForm()
+      } catch (e) {
+        console.log(`[FeedbackFormEditor] ${e}`)
+      }
+      setIsLoading(false)
+    }, [])
+
+    const fetchForm = async () => {
+      if (!entityId)
+        throw Error("Couldn't fetch feedback form: EntityId is required")
+
+      const form = await feedback.getForm({
+        entityId,
+        entityName,
+      })
+      formId = form.id
+      setTitle(form.title)
+      setQuestions(form.questions)
+      setDescription(form.description)
+    }
+
+    const saveForm = async () => {
+      if (!entityId)
+        throw Error("Couldn't save feedback form: EntityId is required")
+
+      setIsLoading(true)
+      const savedForm = formId
+        ? await feedback.updateForm({
+            title,
+            questions,
+            id: formId,
+            description,
+          })
+        : await feedback.createFrom({
+            title,
+            entityId,
+            questions,
+            entityName,
+            description,
+          })
+      onFormSaved?.(savedForm)
+      setIsLoading(false)
+    }
+
+    const handleHeaderChange = (changes: {
+      title?: string
+      description?: string
+    }) => {
+      if (changes.title !== undefined) setTitle(changes.title)
+      if (changes.description !== undefined) setDescription(changes.description)
+    }
+
+    return (
+      <>
+        {!isLoading ? (
+          <div>
+            <h2>Редактор формы обратной связи</h2>
+            <FormHeaderEditor
+              title={title}
+              description={description}
+              onChange={handleHeaderChange}
+            />
+            <QuestionList
+              questions={questions}
+              onChangeQuestions={setQuestions}
+            />
+          </div>
+        ) : (
+          <Loading />
+        )}
+      </>
+    )
+  },
+)
