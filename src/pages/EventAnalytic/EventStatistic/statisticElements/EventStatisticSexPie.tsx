86eea806c7c129936ff033346851839af11a52af
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-06-08 18:23:28 +0400

    tq-98: add no-data

--- src/pages/EventAnalytic/EventStatistic/statisticElements/EventStatisticSexPie.tsx
+++ src/pages/EventAnalytic/EventStatistic/statisticElements/EventStatisticSexPie.tsx
@@ -36,47 +36,51 @@ export const EventStatisticSexPie = ({
       <h6 className="heading_6 event-statistic--Pie__title">
         Распределение участников по половому признаку
       </h6>
-      <ResponsivePie
-        data={data}
-        margin={{ top: 0, right: 10, bottom: 80, left: 10 }}
-        innerRadius={0.2}
-        padAngle={2}
-        cornerRadius={10}
-        activeOuterRadiusOffset={8}
-        arcLinkLabelsSkipAngle={isSmallScreen ? 360 : 10}
-        arcLabelsSkipAngle={10}
-        arcLinkLabelsThickness={3}
-        arcLinkLabelsColor={{ from: 'color' }}
-        colors={{ scheme: 'blues' }}
-        arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
-        arcLabel={datum => {
-          const percent = ((datum.value / total) * 100).toFixed(1)
-          return isSmallScreen
-            ? `${datum.value}`
-            : `${datum.value} (${percent}%)`
-        }}
-        theme={{
-          labels: {
-            text: {
-              fontFamily: 'Montserrat',
-              fontSize: 15,
-              fontWeight: 500,
+      {data.length === 0 ? (
+        <p className="body_l_r event-statistic--Pie__no-data">Нет данных</p>
+      ) : (
+        <ResponsivePie
+          data={data}
+          margin={{ top: 0, right: 10, bottom: 80, left: 10 }}
+          innerRadius={0.2}
+          padAngle={2}
+          cornerRadius={10}
+          activeOuterRadiusOffset={8}
+          arcLinkLabelsSkipAngle={isSmallScreen ? 360 : 10}
+          arcLabelsSkipAngle={10}
+          arcLinkLabelsThickness={3}
+          arcLinkLabelsColor={{ from: 'color' }}
+          colors={{ scheme: 'blues' }}
+          arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
+          arcLabel={datum => {
+            const percent = ((datum.value / total) * 100).toFixed(1)
+            return isSmallScreen
+              ? `${datum.value}`
+              : `${datum.value} (${percent}%)`
+          }}
+          theme={{
+            labels: {
+              text: {
+                fontFamily: 'Montserrat',
+                fontSize: 15,
+                fontWeight: 500,
+              },
             },
-          },
-        }}
-        tooltip={({ datum }) => {
-          const percent = ((datum.value / total) * 100).toFixed(1)
-          return (
-            <div className="event-statistic--Pie__tooltip">
-              <strong>{datum.id}</strong>
-              <br />
-              Количество: {datum.value}
-              <br />
-              Процент: {percent}%
-            </div>
-          )
-        }}
-      />
+          }}
+          tooltip={({ datum }) => {
+            const percent = ((datum.value / total) * 100).toFixed(1)
+            return (
+              <div className="event-statistic--Pie__tooltip">
+                <strong>{datum.id}</strong>
+                <br />
+                Количество: {datum.value}
+                <br />
+                Процент: {percent}%
+              </div>
+            )
+          }}
+        />
+      )}
     </ContainerBox>
   )
 }
