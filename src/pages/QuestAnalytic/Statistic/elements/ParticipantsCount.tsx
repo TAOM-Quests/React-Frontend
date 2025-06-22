0541fe9d0665a4d738e8cb5c5e4737136ea258f7
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2025-06-22 15:45:26 +0400

    tq-112: fix name

--- src/pages/QuestAnalytic/Statistic/elements/ParticipantsCount.tsx
+++ src/pages/QuestAnalytic/Statistic/elements/ParticipantsCount.tsx
@@ -7,9 +7,7 @@ export const QuestStatisticParticipantCount = ({
 }: QuestAnalyticElementProps) => {
   return (
     <ContainerBox>
-      <p className="body_m_r statistic__cards--title">
-        Количество зарегистрированных участников
-      </p>
+      <p className="body_m_r statistic__cards--title">Прошли до конца</p>
       <div className="statistic__participant-count">
         <p className="heading_4">{participants.length}</p>
         <Icon icon="USER" />
